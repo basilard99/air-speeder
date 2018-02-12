254f1c6ecbd3dfa3cd94fcf0d3d96aaf6994987e
Chris Snyder <basilard99@gmail.com>
2018-02-12 15:59:26 -0500

    Finished the first round of coding for the metrics
    The metrics writer now outputs the header based on the type of metrics
    that are being passed in.

--- src/writemetricscommand.py
+++ src/writemetricscommand.py
@@ -1,11 +1,16 @@
 import csv
-import inspect
 
 class WriteMetricsCommand:
     def __init__(self, filepath):
         self.__filepath = filepath
 
+    def getattributes(self, metricsdefinition):
+        boring = dir(type('dummyObject', (object,), {}))
+        return [item
+                for item in inspect.getmembers(metricsdefinition)
+                if item[0] not in boring]
+
     def writeheader(self, metricsdefinition):
         with open(self.__filepath, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow()
+            writer.writerow(metricsdefinition.__dict__.keys())
